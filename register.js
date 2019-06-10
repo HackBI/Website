f1a666b42984d6175c63c5224553eb02bf8641af
maxdodson <Maxwell.Dodson@bishopireton.org>
2019-06-10 04:42:32 -0400

    registration work

--- register.js
+++ register.js
@@ -20,23 +20,29 @@ $(document).ready(function() {
 		updateBgs();
 	});
 
-	/* $('#wave1').wavify({
-		height: 230,
-		bones: 4,
-		amplitude: -30,
-		color: 'rgba(255, 255, 255, 1)',
-		speed: .15
-	  });
-	$(".wave-container").css({'opacity': '0'}).animate({'opacity': '.99'}, 5000); */
+	$("#file-1").change(function(){
+		console.log('File(s) inserted.');
+		try {
+			for (var i=0; i < this.files.length; i++) {
+				$('.content').append($('template').html().replace('<div class="name"></div>','<div class="name">' + this.files[i].name + '</div>'));
+				uploaded_files.push(this.files[i]);
+			}
+		} catch {
+			alert("Something didn't work quite right. Try again.");
+			location.reload();
+		}
+	});
+
+	uploaded_files = [];
+	ajaxresponses = 0;
 	
 	var clicked = 0;
 
 	$('a[href^="#eventbrite"]').on('click',function (e) {
 		
 	    if (clicked != 0) {
-		return;
+			return;
 	    }
-		
 	    clicked++;
 		
 	    e.preventDefault();
@@ -80,3 +86,80 @@ $(document).ready(function() {
 
 	});
 });
+
+function dropHandler(ev) {
+	console.log('File(s) dropped');
+	ev.preventDefault();
+  
+	if (ev.dataTransfer.items) {
+	  // Use DataTransferItemList interface to access the file(s)
+	  for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+		// If dropped items aren't files, reject them
+		if (ev.dataTransfer.items[i].kind === 'file') {
+		  var file = ev.dataTransfer.items[i].getAsFile();
+		  console.log('... file[' + i + '].name = ' + file.name);
+		  $('.content').append($('#filename').html().replace('<div class="name"></div>','<div class="name">' + file.name + '</div>'));
+		  uploaded_files.push(file);
+		}
+	  }
+	} else {
+		// Use DataTransfer interface to access the file(s)
+		for (var i = 0; i < ev.dataTransfer.files.length; i++) {
+			console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
+			$('.content').append($('template').html().replace('<div class="name"></div>','<div class="name">' + ev.dataTransfer.files[i].name + '</div>'));
+			uploaded_files.push(ev.dataTransfer.files[i]);
+		}
+	}
+}
+  
+function dragOverHandler(ev) {
+	console.log('File(s) in drop zone'); 
+	ev.preventDefault();
+}
+  
+function doPost() { 
+	try {
+		for (var i = 0; i < uploaded_files.length; i++) {
+			var file = uploaded_files[i];
+			var fr = new FileReader();
+			fr.fileName = file.name;
+		
+			var data;
+			var mimetype;
+			var filename;
+	  
+			fr.onloadend = function(e) {
+				data = e.target.result.replace(/^.*,/, '');
+				mimetype = e.target.result.match(/^.*(?=;)/)[0];
+				filename = e.target.fileName;
+	
+				var fd = new FormData();
+				fd.append('data', data);
+				fd.append('mimetype', mimetype);
+				fd.append('filename', filename);
+			
+				$.ajax({
+					type: 'POST',
+					url: 'https://script.google.com/macros/s/AKfycbzDg5C5xQnFNwwlqQoinEZNJPGMBDhNDq6V96uC2z6Doby088Q/exec',
+					data: fd,
+					crossDomain: true,
+					contentType: false,
+					processData: false,
+					success: updateStatus
+				});
+			}
+			fr.readAsDataURL(file);
+		}
+	} catch {
+		alert("Something didn't work quite right. Try again.");
+	}
+}
+  
+function updateStatus() {
+	ajaxresponses++;
+	if (ajaxresponses == uploaded_files.length) {
+	  $('#submitbtn').text('Submitted.');
+	  $('.file').remove();
+	  uploaded_files.length = 0;
+	}
+}
