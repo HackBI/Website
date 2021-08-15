0d2edd07ab6c3c3ac7c00d8f4d7dcb94f41d6ebd
Natalie Ramirez <61169728+natalieramirez22@users.noreply.github.com>
2021-08-15 13:06:02 -0400

    fix images in teams page and proper indentation across codebase

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 function contactSubmit() {
-	
-	var name = escape($('#namef1').val()); //.replace(/\s/g, "+"));
-	var email = escape($('#emailf1').val()); //.replace(/\s/g, "+"));
-	var question = escape($('#contactq1').val()); //.replace(/\s/g, "+"));
+  var name = escape($("#namef1").val()); //.replace(/\s/g, "+"));
+  var email = escape($("#emailf1").val()); //.replace(/\s/g, "+"));
+  var question = escape($("#contactq1").val()); //.replace(/\s/g, "+"));
 
-	if (question == "" || name == "" || email == "") {
-		alert("Please enter a value for all fields");
-	}
-	else {
-		emailjs.init("user_mWOtaoxzxeJyW8OxdfwGS");
-	let contactParams = {
-		from_name: name,
-		from_email: email,
-		message: question
-	};
-	emailjs.send("service_9403v3m", "template_v3e8pel", contactParams).then(function (res){})
-		/*
+  if (question == "" || name == "" || email == "") {
+    alert("Please enter a value for all fields");
+  } else {
+    emailjs.init("user_mWOtaoxzxeJyW8OxdfwGS");
+    let contactParams = {
+      from_name: name,
+      from_email: email,
+      message: question,
+    };
+    emailjs
+      .send("service_9403v3m", "template_v3e8pel", contactParams)
+      .then(function (res) {});
+    /*
 		$.ajax({
 			headers: {
         			'Content-Type': 'application/x-www-form-urlencoded'
@@ -41,59 +41,61 @@ function contactSubmit() {
 			}
 		});
 			*/
-		console.log("sent message");
-	}
-	return;
+    console.log("sent message");
+  }
+  return;
 }
 
 function preregisterSubmit() {
-	$('#volbtnsubmit').prop('disabled', 'true');
-	var name = escape($('#namef').val().replace(/\s/g, "+"));
-	var email = escape($('#emailf').val().replace(/\s/g, "+"));
-	
-	$.ajax({
-		'type': 'POST',
-		'url': `https://script.google.com/macros/s/AKfycbxK5zr2m51a5ARZdjipIFJ4P3XKkyjM-Yi64VhcalGLLraeetc/exec?function=preregister&name=${name}&email=${email}`,
-		crossDomain: true,
-		contentType: false,
-		dataType: 'text/javascript',
-		processData: false,
-		beforeSend: function() {
-			$('#volbtnsubmit').prop('disabled','true');
-			$('#volbtnsubmit').html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true" style="padding: 7px"></span>&ensp;Working..');
-},
-		statusCode: {
-			200: function() {
-$('#volbtnsubmit').text('Success!');
-				setTimeout(function() {
-$('#preregisterModal').modal('hide');
-				}, 1500);
-			}
-		}
-	});
+  $("#volbtnsubmit").prop("disabled", "true");
+  var name = escape($("#namef").val().replace(/\s/g, "+"));
+  var email = escape($("#emailf").val().replace(/\s/g, "+"));
 
+  $.ajax({
+    type: "POST",
+    url: `https://script.google.com/macros/s/AKfycbxK5zr2m51a5ARZdjipIFJ4P3XKkyjM-Yi64VhcalGLLraeetc/exec?function=preregister&name=${name}&email=${email}`,
+    crossDomain: true,
+    contentType: false,
+    dataType: "text/javascript",
+    processData: false,
+    beforeSend: function () {
+      $("#volbtnsubmit").prop("disabled", "true");
+      $("#volbtnsubmit").html(
+        '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true" style="padding: 7px"></span>&ensp;Working..'
+      );
+    },
+    statusCode: {
+      200: function () {
+        $("#volbtnsubmit").text("Success!");
+        setTimeout(function () {
+          $("#preregisterModal").modal("hide");
+        }, 1500);
+      },
+    },
+  });
 }
 
 function clearForm() {
-	document.getElementById("contactform").reset();
-	if (registerReq != 0) {
-		$('#contactq').prop('readonly', false);
-		$('#contactq').prop('placeholder', 'Question');
-		document.getElementById("emailf").classList.remove('contacthl');
-		document.getElementById("namef").classList.remove('contacthl');
-		document.getElementById("contactq").classList.remove('contactc');
-		registerReq = 0;
-	}
-	return;
+  document.getElementById("contactform").reset();
+  if (registerReq != 0) {
+    $("#contactq").prop("readonly", false);
+    $("#contactq").prop("placeholder", "Question");
+    document.getElementById("emailf").classList.remove("contacthl");
+    document.getElementById("namef").classList.remove("contacthl");
+    document.getElementById("contactq").classList.remove("contactc");
+    registerReq = 0;
+  }
+  return;
 }
 
-$(document).ready(function() {
-	if (window.innerWidth > 767) { // Load video if on desktop
-		$('video').append('<source src="HackBIShort.mp4" type="video/mp4">');
-		$('video').css('opacity', '.89');
-		$('video').get(0).load();
-	}
-	else { // Else keep poster image
-		$('video').css('opacity', '.95');
-	}
+$(document).ready(function () {
+  if (window.innerWidth > 767) {
+    // Load video if on desktop
+    $("video").append('<source src="HackBIShort.mp4" type="video/mp4">');
+    $("video").css("opacity", ".89");
+    $("video").get(0).load();
+  } else {
+    // Else keep poster image
+    $("video").css("opacity", ".95");
+  }
 });
